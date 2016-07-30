3e3bfe56571ecc13262fc92222af6232fb99ef47
Abdul Hafidz A <aditans88@gmail.com>
2016-07-30 20:30:02 +0700

    revisi task per user

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -30,7 +30,7 @@ class HomeController extends Controller
      */
     public function index()
     {   $dn = \Carbon\Carbon::now();
-        $tasks = $this->tasks->where('status', 1)->whereBetween('date', [$dn->subHour()->format('Y-m-d H:i:s'), $dn->addHour()->format('Y-m-d H:i:s')])->get();
+        $tasks = $this->tasks->where('user_id', auth()->user()->id )->where('status', 1)->whereBetween('date', [$dn->subHour()->format('Y-m-d H:i:s'), $dn->addHour()->format('Y-m-d H:i:s')])->get();
         return view('dashboard.main', compact('tasks'));
     }
 }
